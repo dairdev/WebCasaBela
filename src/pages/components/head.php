b2f21519ae246e8d0e8133bddacbd0722eeb01cf
Dennis <dennis.dair@gmail.com>
2024-02-18 22:36:47 -0500

    separate head in a component

---
+++ src/pages/components/head.php
@@ -0,0 +1,18 @@
+<title>Casabela</title>
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width" />
+<link rel="apple-touch-icon" sizes="180x180" href="assets/icons/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="assets/icons/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="assets/icons/favicon-16x16.png">
+<link rel="manifest" href="/site.webmanifest">
+<link rel="mask-icon" href="assets/icons/safari-pinned-tab.svg" color="#5bbad5">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="theme-color" content="#ffffff">
+<!--
+Need a visual blank slate?
+Remove all code in `styles.css`!
+https://kitwind.io/products/kometa/components/navs
+-->
+<link rel="stylesheet" href="assets/css/style.css" />
+<link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+
