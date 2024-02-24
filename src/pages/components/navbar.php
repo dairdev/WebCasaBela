ba071999e6bdcc6993e317669fe5c7a97d5367a8
Dennis <dennis.dair@gmail.com>
2024-02-24 11:07:56 -0500

    change tag nav by div

--- src/pages/components/navbar.php
+++ src/pages/components/navbar.php
@@ -1,4 +1,4 @@
-<nav class="bg-white bg-opacity-35">
+<div class="bg-white bg-opacity-35">
   <div class="px-4 py-8 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
     <div class="relative flex grid items-center grid-cols-2 lg:grid-cols-3">
       <menu class="flex items-center hidden space-x-8 lg:flex">
@@ -40,4 +40,4 @@
       </div>
     </div>
   </div>
-</nav>
+</div>
