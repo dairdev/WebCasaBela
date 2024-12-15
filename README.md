12c64032f5c36bf3c45138f43e66b2f2ab8fb7f5
Dennis <dennis.dair@gmail.com>
2024-12-15 17:56:05 -0500

    Clean repo

--- README.md
+++
@@ -1,5 +0,0 @@
-# WebCasaBela
-
-Webpage for CasaBela &reg; which is a Real Statement company
-
-Website made using PHP and Tailwindcss
--- updatelinks.sh
+++
@@ -1,2 +0,0 @@
-find public/. -type f -name '*.html' | xargs sed -i 's/src\/pages/public/g'
-find public/. -type f -name '*.html' | xargs sed -i 's/.php/.html/g'
