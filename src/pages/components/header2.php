d08a98f9a3522684ea4400f6b2529e0ed45f5012
Dennis <dennis.dair@gmail.com>
2024-12-15 18:43:37 -0500

    Delete old folder

--- src/pages/components/header2.php
+++
@@ -1,8 +0,0 @@
-<header class="relative h-auto">
-  <div class="relative">
-    <img src="assets/images/headerbackground1.jpg" class="absolute inset-0 object-cover w-full h-full" alt="" />
-    <div class="relative bg-gray-900 bg-opacity-75">
-      <?php include __DIR__."/navbar.php"  ?>
-    </div>
-  </div>
-</header>
